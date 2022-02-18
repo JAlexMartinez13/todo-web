a1e20096c4e129634deadc46e1834ad226ab39b0
J Alexander Martinez <Jalexmartinez1013@hotmail.com>
2022-02-18 10:58:17 -0500

    connected app to firestore

--- src/components/Task.js
+++ src/components/Task.js
@@ -3,6 +3,16 @@ import { List } from 'antd'
 
 export default function Task({ item }){
     const [itemStyle, setItemStyle] = useState({})
-    useEffect(()=>{}, [])
+    useEffect(()=>{
+        if(item.done) {
+            setItemStyle({color: 'grey', textDecoration: 'line-through'});
+        }else{
+            setItemStyle({
+                color: 'black',
+                textDecoration: 'none'
+            })
+        }
+            
+    }, [item])
     return <List.Item style={itemStyle}>{item.task}</List.Item>
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,16 +1,22 @@
 import { List } from 'antd';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Task from './Task'
 
 const fakeTasks = [
-    { id: 1, task: 'Grow Limes', done: false},
-    { id: 2, task: 'Drink Beer', done: false},
-    { id: 3, task: 'Buy Wine', done: false},
+    { id: 1, task: 'Grow Limes', done: true},
+    { id: 2, task: 'Drink Beer', done: true},
+    { id: 3, task: 'Buy Wine', done: true},
     { id: 4, task: 'Buy Tequila', done: false},
     { id: 5, task: 'Buy Food', done: false},
 ]
 export default function TaskList(){
     const [tasks, setTasks] = useState(fakeTasks)
+    useEffect(()=>{
+        fetch('https://much-todo-am.uc.r.appspot.com/tasks')
+        .then(response => response.json())
+        .then(data => setTasks(data))
+        .catch(alert)
+    })
     return (
         <List 
         bordered
